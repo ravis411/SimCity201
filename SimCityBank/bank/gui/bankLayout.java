9f573c2a2d77d85b6bfb3fcd965061dcb62f1574
Byron Choy <byroncho@usc.edu>
2013-11-19 17:07:06 -0800

    GUI incomplete but role design matches design docs

---
+++ SimCityBank/bank/gui/bankLayout.java
@@ -0,0 +1,14 @@
+package bank.gui;
+
+import bank.*;
+import astar.*;
+import java.util.concurrent.*; 
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Vector;
+public class bankLayout {
+
+	
+}
