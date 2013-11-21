a96ad67aa697693cfae1656319ddc25e155a2b64
Luca Spinazzola <spinazzo@usc.edu>
2013-11-20 19:54:46 -0800

    Basic Layout For Gui Done

---
+++ src/market/gui/MarketAnimationPanel.java
@@ -0,0 +1,106 @@
+package market.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class MarketAnimationPanel extends JPanel implements ActionListener {
+
+    private final static int WINDOWX = 800;//
+    private final static int WINDOWY = 800;
+    final static int storeCounterX = 0;//TABLEX and TABLEY describe where the table appears in the panel
+    final static int storeCounterY = 400;
+    final static int storeCounterWIDTH = 800;
+    final static int storeCounterXHEIGHT = 50;
+    
+    final static int TIMERCOUNTmilliseconds = 5;
+    private Image bufferImage;
+    private Dimension bufferSize;
+
+    private List<Gui> guis = new ArrayList<Gui>();
+
+    public MarketAnimationPanel() {
+    	setSize(WINDOWX, WINDOWY);
+        setVisible(true);
+        
+        bufferSize = this.getSize();
+ 
+    	Timer timer = new Timer(TIMERCOUNTmilliseconds, this );
+    	timer.start();
+    }
+
+	public void actionPerformed(ActionEvent e) {
+		repaint();  //Will have paintComponent called
+	}
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+
+        //Clear the screen by painting a rectangle the size of the frame
+        g2.setColor(getBackground());
+        g2.fillRect(0, 0, WINDOWX, WINDOWY );
+
+        
+        g2.setColor(Color.ORANGE);
+        
+        g2.fillRect(storeCounterX, storeCounterY, storeCounterWIDTH, storeCounterXHEIGHT);//store counter
+
+        g2.fillRect(0,200,200,20);//top manager office wall
+        g2.fillRect(200,200,20,100);//manager office top half right wall
+        g2.fillRect(200,340,20,60);//manager office bottom half left wall
+        g2.fillRect(400, 0, 30, 250);//shelve #1 from left
+        g2.fillRect(500, 0, 30, 250);//shelve #2 from left
+        g2.fillRect(600, 0, 30, 250);//shelve #3 from left
+        g2.fillRect(700, 0, 30, 250);//shelve #4 from left
+        
+        g2.setColor(Color.BLACK);//sets text color...or anything following this line
+        g2.setFont(new Font("Serif", Font.PLAIN, 15));
+        g2.fillRect(350, 400, 10, 50);//dividers between counter windows 
+        g2.drawString("Window #1", 370, 430);
+        g2.fillRect(450, 400, 10, 50);
+        g2.drawString("Window #2", 470, 430);
+        g2.fillRect(550, 400, 10, 50);
+        g2.drawString("Window #3", 570, 430);
+        g2.fillRect(650, 400, 10, 50);
+        
+       
+		g2.drawString("Manager's Office", 60, 215);
+		g2.drawString("Store Shelves Area", 510, 20);
+		g2.drawString("Delivery Truck", 10, 20);
+        for(Gui gui : guis) {
+            if (gui.isPresent()) {
+                gui.updatePosition();
+            }
+        }
+
+        for(Gui gui : guis) {
+            if (gui.isPresent()) {
+                gui.draw(g2);
+            }
+        }
+    }
+/*
+    public void addGui(CustomerGui gui) {
+        guis.add(gui);
+    }
+
+    public void addGui(WaiterGui gui) {
+        guis.add(gui);
+    }
+
+	public void addGui(CookGui gui) {
+		guis.add(gui);
+		
+	}
+	*/
+}
--- src/market/gui/MarketGui.java
+++ src/market/gui/MarketGui.java
@@ -1,71 +1,24 @@
 package market.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-
-import market.gui.Gui;
 
 import javax.swing.JFrame;
-import javax.swing.Timer;
-
-import restaurant.gui.CustomerGui;
 /**
  * Main GUI class.
  * Contains the main frame and subsequent panels
  */
 public class MarketGui extends JFrame implements ActionListener {
-    /* The GUI has two frames, the control frame (in variable gui) 
-     * and the animation frame, (in variable animationFrame within gui)
-     */
-	
-	//AnimationPanel animationPanel = new AnimationPanel();
-	
-    /* restPanel holds 2 panels
-     * 1) the staff listing, menu, and lists of current customers all constructed
-     *    in RestaurantPanel()
-     * 2) the infoPanel about the clicked Customer (created just below)
-     */    
-  //  private RestaurantPanel restPanel = new RestaurantPanel(this);
-    
-    /* infoPanel holds information about the clicked customer, if there is one*/
-    /*private JPanel infoPanel;
-    private JLabel infoLabel; //part of infoPanel
-    private JCheckBox stateCB;//part of infoLabel
-    private JPanel lucaPanel; //Luca's panel below info
-    private Icon lucaIcon;	  //part of lucapanel
-    private JPanel pausePanel; 
-    private JLabel addTableLabel;
-    private JButton pauseButton;
-    private JPanel leftPanel;
-    private boolean paused =false;
-    public Semaphore waiterOnBreak = new Semaphore(0,false);
-
-    private Object currentPerson;/* Holds the agent that the info is about.
-    								Seems like a hack */
-
+   
+	MarketAnimationPanel animationPanel = new MarketAnimationPanel();
 	final int WINDOWX = 800;// window width
     final int WINDOWY = 800;//window height
     final int WINDOWXOpenPosition = 50;//how many pixels from top left of screen window will appear
     final int WINDOWYOpenPosition = 50;
-    final int TABLEX = 200;//TABLEX and TABLEY describe where the table appears in the panel
-    final int TABLEY = 250;
-    final int TABLEWIDTH = 50;
-    final int TABLEHEIGHT = 50;
-    final static int TIMERCOUNTmilliseconds = 5;
-    private Image bufferImage;
-    private Dimension bufferSize;
-
-    private List<Gui> guis = new ArrayList<Gui>();
-
+  
 
     /**
      * Constructor for MarketGui class.
@@ -84,34 +37,12 @@ public class MarketGui extends JFrame implements ActionListener {
         setPreferredSize(marketAnimationFrameDim);
         setMinimumSize(marketAnimationFrameDim);
         setMaximumSize(marketAnimationFrameDim);
-        bufferSize = this.getSize();
-        Timer timer = new Timer(TIMERCOUNTmilliseconds, this );
-    	timer.start();
-       
-      
-
+        add(animationPanel, BorderLayout.CENTER);
     }
     
     
     public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
-
-        //Clear the screen by painting a rectangle the size of the frame
-        g2.setColor(getBackground());
-        g2.fillRect(0, 0, WINDOWX, WINDOWY );
-
-        //Here is the table
-        g2.setColor(Color.ORANGE);
         
-        g2.fillRect(TABLEX, TABLEY, TABLEWIDTH, TABLEHEIGHT);//200 and 250 need to be table params
-        
-        
-        g2.setColor(Color.BLACK);
-        
-        g2.setFont(new Font("Serif", Font.PLAIN, 15));
-		g2.drawString("Plating Area", 450, 120);
-		g2.drawString("Cooking Area", 450, 20);
-		g2.drawString("Refrigerator", 300, 20);
         /*for(Gui gui : guis) {
             if (gui.isPresent()) {
                 gui.updatePosition();
@@ -191,12 +122,10 @@ public class MarketGui extends JFrame implements ActionListener {
      */
     
     public void actionPerformed(ActionEvent e) {
-    	repaint();  //Will have paintComponent called
+    	//
     }
     
-    public void addGui(MarketEmployeeGui gui) {
-        guis.add(gui);
-    }
+
     
     
     /**
