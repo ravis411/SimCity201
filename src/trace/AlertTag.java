b855e82f50c48c089b08611e5450c53a3cb8f5fb
ryanwdav@usc.edu <ryanwdav@usc.edu>
2013-11-23 17:09:48 -0800

    added a tag the bin/ is really obnoxious

--- src/trace/AlertTag.java
+++ src/trace/AlertTag.java
@@ -20,5 +20,6 @@ public enum AlertTag {
 	BUS_STOP,
 	RESTAURANT,		//For the demo code where you make a new restaurant
 	BANK,			//For the demo code where you make a new bank
-	GENERAL_CITY
+	GENERAL_CITY,
+	VEHICLE_GUI
 }
