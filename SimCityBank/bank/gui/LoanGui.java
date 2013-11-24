540c111e8a6452001330347ecf03b64b0111816d
Byron Choy <byroncho@usc.edu>
2013-11-23 17:21:13 -0800

    Used Luca's GUI as a base

---
+++ SimCityBank/bank/gui/LoanGui.java
@@ -0,0 +1,25 @@
+package bank.gui;
+
+import java.awt.Graphics2D;
+
+public class LoanGui implements Gui {
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
