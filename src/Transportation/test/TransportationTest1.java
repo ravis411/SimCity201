1d547e2b473565031be591e57a55e572e15cac57
Jeffrey Chau <chaujeff@usc.edu>
2013-11-25 11:45:28 -0800

    Reorganizing projects for Unit testing in Transportation. Workarond hacks

---
+++ src/Transportation/test/TransportationTest1.java
@@ -0,0 +1,19 @@
+package Transportation.test;
+
+import Transportation.BusAgent;
+import junit.framework.TestCase;
+
+public class TransportationTest1 extends TestCase {
+	BusAgent bus;
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		bus = new BusAgent("Bus_1");
+	}
+	
+	
+	public void standardBusRouteScenarioOne() {
+		
+	}
+	
+}
