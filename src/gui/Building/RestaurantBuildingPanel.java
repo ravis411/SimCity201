710e474b55887927a91e91dbaecdab81331af387
Dylan Resnik <dylanres@usc.edu>
2013-11-26 06:04:04 -0800

    Restaurants can now be added to overworld.

---
+++ src/gui/Building/RestaurantBuildingPanel.java
@@ -0,0 +1,64 @@
+package gui.Building;
+
+import gui.BuildingsPanels;
+import interfaces.GuiPanel;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JLabel;
+
+import residence.gui.AnimationPanel;
+import restaurant.gui.RestaurantAnimationPanel;
+import Person.Role.Role;
+
+
+/**
+ * Default class for various gui building panels.
+ * BuildingPanel is added to the buildingPanels and is displayed in the zoomed in view of a building.
+ * 
+ *
+ */
+public class RestaurantBuildingPanel extends BuildingPanel{
+	
+	RestaurantAnimationPanel restaurantPanel;
+	
+	public RestaurantBuildingPanel(Rectangle2D r, String name, BuildingsPanels buildingPanels) {
+		super(r, name, buildingPanels);
+		
+		this.removeAll();
+		
+		setBackground( Color.yellow );
+		
+		restaurantPanel = new RestaurantAnimationPanel();
+		
+		setLayout(new GridLayout(1,1));
+		
+		
+		JLabel j = new JLabel( myName );
+		add( restaurantPanel );
+	}
+	
+	
+	public String getName() {
+		return myName;
+	}
+	
+	public RestaurantAnimationPanel getPanel() {
+		return restaurantPanel;
+	}
+
+	public void displayBuildingPanel() {
+		myCity.displayBuildingPanel( this );
+		
+	}
+
+
+	@Override
+	public void addPersonWithRole(Role r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/gui/SetUpWorldFactory.java
+++ src/gui/SetUpWorldFactory.java
@@ -13,6 +13,8 @@ import gui.Building.MarketBuilding;
 import gui.Building.MarketBuildingPanel;
 import gui.Building.ResidenceBuilding;
 import gui.Building.ResidenceBuildingPanel;
+import gui.Building.RestaurantBuilding;
+import gui.Building.RestaurantBuildingPanel;
 import gui.MockAgents.PseudoBusAgent;
 import gui.agentGuis.PersonGui;
 import gui.agentGuis.VehicleGui;
@@ -663,6 +665,15 @@ public class SetUpWorldFactory{
 			mb.setBuildingPanel(mp);
 			cityPanel.addGui(mb);
 			buildingsPanels.addBuildingPanel(mp);
+		}
+			break;
+		case "Restaurant":
+			RestaurantBuilding restb = new RestaurantBuilding(building);
+			if(restb != null){
+			RestaurantBuildingPanel restPanel = new RestaurantBuildingPanel(restb, name, buildingsPanels);
+			restb.setBuildingPanel(restPanel);
+			cityPanel.addGui(restb);
+			buildingsPanels.addBuildingPanel(restPanel);
 		}
 			break;
 		default:
