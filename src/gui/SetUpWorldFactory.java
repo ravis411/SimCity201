114994ce1917e50de9066713480905747c72f3f4
ryanwdav@usc.edu <ryanwdav@usc.edu>
2013-11-26 08:43:58 -0800

    wat

--- src/gui/SetUpWorldFactory.java
+++ src/gui/SetUpWorldFactory.java
@@ -222,11 +222,11 @@ public class SetUpWorldFactory{
 			
 			addPerson("Person 1", buildingsPanels.getResidenceBuildingPanel("House 1"));
 			addPerson("Person 2", buildingsPanels.getResidenceBuildingPanel("House 2"));
-			addPerson("Person 3", buildingsPanels.getResidenceBuildingPanel("House 2"));
-			addPerson("Person 4", buildingsPanels.getResidenceBuildingPanel("House 2"));
-			addPerson("Person 5", buildingsPanels.getResidenceBuildingPanel("House 2"));
-			addPerson("Person 6", buildingsPanels.getResidenceBuildingPanel("House 2"));
-			addPerson("Person 7", buildingsPanels.getResidenceBuildingPanel("House 2"));
+		//	addPerson("Person 3", buildingsPanels.getResidenceBuildingPanel("House 2"));
+		//	addPerson("Person 4", buildingsPanels.getResidenceBuildingPanel("House 2"));
+		//	addPerson("Person 5", buildingsPanels.getResidenceBuildingPanel("House 2"));
+		//	addPerson("Person 6", buildingsPanels.getResidenceBuildingPanel("House 2"));
+		//	addPerson("Person 7", buildingsPanels.getResidenceBuildingPanel("House 2"));
 		
 	} //end LoadDefault
 	
