804f6ede67ed2908066b3746ffc2935b045653e3
maciesielka <maciesielka@comcast.net>
2013-12-08 15:55:54 -0800

    New XML Parser in the works

--- src/gui/SetUpWorldFactory.java
+++ src/gui/SetUpWorldFactory.java
@@ -29,10 +29,20 @@ import gui.agentGuis.PersonGui;
 import gui.agentGuis.VehicleGui;
 
 import java.awt.Dimension;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import residence.HomeRole;
 import trace.AlertLog;
@@ -773,5 +783,101 @@ public class SetUpWorldFactory{
 		}
 		locationMap.add(new LocationInfo(location));
 	}
-	
+
+	public void loadXMLFile(String filepath){
+		try{
+			File xmlFile = new File(this.getClass().getResource(filepath).toURI());
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
+			//setup the document for easier reading
+			doc.getDocumentElement().normalize();
+			NodeList masterList = doc.getDocumentElement().getChildNodes();
+			for(int i = 0; i < masterList.getLength(); i++){
+				Node iNode = masterList.item(i);
+				if(iNode.getNodeType() == Node.ELEMENT_NODE){
+					Element iElement = (Element) iNode;
+					switch(iElement.getNodeName()){
+						case Config.PERSON_NODE:
+							//get attributes
+							String iName = null, iHome = null, iJob = null, iLocation = null;
+							String iShift = null;
+							boolean iHasCar = false;
+					
+							NamedNodeMap iMap = iElement.getAttributes();
+							for(int j = 0; j < iMap.getLength(); j++){
+								Node jNode = iMap.item(j);
+								switch(jNode.getNodeName()){
+									case Config.NAME_ATTRIBUTE:
+										iName = jNode.getNodeValue();
+										break;
+									case Config.HOME_ATTRIBUTE:
+										iHome = jNode.getNodeValue();
+										break;
+									case Config.JOB_ATTRIBUTE:
+										iJob = jNode.getNodeValue();
+										break;
+									case Config.LOCATION_ATTRIBUTE:
+										iLocation = jNode.getNodeValue();
+										break;
+									case Config.CAR_ATTRIBUTE:
+										iHasCar = Boolean.parseBoolean(jNode.getNodeValue());
+									case Config.SHIFT_ATTRIBUTE:
+										iShift = jNode.getNodeValue();
+										break;
+								}
+							}
+							
+							System.out.println("Person Name="+iName+", Home="+iHome+", Job="+iJob+
+									", Location="+iLocation+", HasCar="+iHasCar);
+							//get children
+							NodeList personChildren = iElement.getChildNodes();
+							for(int j = 0; j < personChildren.getLength(); j++){
+								Node jNode = personChildren.item(j);
+								if(jNode.getNodeType() == Node.ELEMENT_NODE){
+									Element jElement = (Element) jNode;
+									//get attributes
+									String jName = null;
+									//get children
+									switch(jElement.getNodeName()){
+										
+										case Config.FRIENDS_NODE:
+											//get attributes
+											//get children
+											NodeList jChildren = jElement.getChildNodes();
+											for(int k = 0; k < jChildren.getLength(); k++){
+												Node kNode = jChildren.item(k);
+												if(kNode.getNodeType() == Node.ELEMENT_NODE){
+													Element kElement = (Element) kNode;
+													switch(kNode.getNodeName()){
+														case Config.FRIEND_NODE:
+															//get attributes
+															String kName = null;
+															NamedNodeMap kMap = kElement.getAttributes();
+															for(int l = 0; l < kMap.getLength(); l++){
+																Node lNode = kMap.item(l);
+																switch(lNode.getNodeName()){
+																	case Config.NAME_ATTRIBUTE:
+																		kName = lNode.getNodeValue();
+																		break;
+																}
+															}
+															System.out.println("\t"+kName);
+															break;
+													}
+												}
+											}
+									}
+									
+								}
+							}
+							break;
+					}
+				}
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
