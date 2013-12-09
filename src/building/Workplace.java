cf54ebb3ffaf1f7d829357ac4fbe9919bfb825d5
maciesielka <maciesielka@comcast.net>
2013-12-09 08:06:13 -0800

    Made Workplace an abstract class

--- src/building/Workplace.java
+++ src/building/Workplace.java
@@ -1,13 +1,22 @@
 package building;
 
+import interfaces.generic_interfaces.GenericCashier;
+import interfaces.generic_interfaces.GenericCook;
+import interfaces.generic_interfaces.GenericHost;
+import interfaces.generic_interfaces.GenericWaiter;
+import ryansRestaurant.RyansCookRole;
 import gui.Building.BuildingPanel;
+import Person.Role.Employee;
+import Person.Role.Role;
 
 public abstract class Workplace extends Building{
 	
-	protected Workplace(BuildingPanel panel) {
+	public Workplace(BuildingPanel panel) {
 		super(panel);
 		// TODO Auto-generated constructor stub
 	}
+	
+	private boolean ready = false;
 
 	public static int DAY_SHIFT_HOUR = 9;
 	public static int DAY_SHIFT_MIN = 0;
@@ -35,5 +44,66 @@ public abstract class Workplace extends Building{
 	public abstract boolean isOpen();
 	
 	public abstract void notifyEmployeesTheyCanLeave();
+	
+	public void getReadyForWork(){
+		for(Role role : inhabitants){
+			if(role instanceof Employee){
+				Employee e = (Employee) role;
+				
+				if(e instanceof GenericWaiter){
+					GenericWaiter gw = (GenericWaiter) e;
+					//System.out.println(e.getWorkLocation());
+					Restaurant rest = (Restaurant) BuildingList.findBuildingWithName(e.getWorkLocation());
+					gw.setHost(rest.getHostRole());
+					rest.getHostRole().addWaiter(gw);
+					gw.setCashier(rest.getCashierRole());
+					gw.setCook(rest.getCookRole());
+					//return gw;
+				}else if(e instanceof GenericHost){
+					GenericHost gh = (GenericHost) e;
+					Restaurant rest = (Restaurant) BuildingList.findBuildingWithName(e.getWorkLocation());
+					//return gh;
+				}else if(e instanceof GenericCook){
+					GenericCook gc = (GenericCook) e;
+					if(gc instanceof RyansCookRole){
+						RyansCookRole rcr = (RyansCookRole) gc;
+						Restaurant rest = (Restaurant) BuildingList.findBuildingWithName(rcr.getWorkLocation());
+						rcr.setCashier(rest.getCashierRole());
+					}
+					//return gc;
+				}else if(e instanceof GenericCashier){
+					GenericCashier gc = (GenericCashier) e;
+					//return gc;
+				}else{
+					//return e;
+				}
+				
+			}
+		}
+		
+		for(Role r : inhabitants){
+			System.err.println("Activating: " + r.getNameOfRole() );
+			r.getPerson().workIsOpen();
+		}
+	}
+
+	@Override
+	public void addRole(Role r) {
+		// TODO Auto-generated method stub
+		super.addRole(r);	
+		if(isOpen() && !ready){
+			ready = true;
+			getReadyForWork();
+		}
+	}
+	
+	public void activateAll(){
+		
+		for(Role r : inhabitants){
+			r.activate();
+		}
+	}
+	
+	
 	
 }
