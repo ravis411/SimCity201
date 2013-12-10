a121222323141a92887c6dd945b1f4947dd4f5b9
Byron Choy <byroncho@usc.edu>
2013-12-09 17:41:09 -0800

    Setting up merge with develop

--- src/gui/SetUpWorldFactory.java
+++ src/gui/SetUpWorldFactory.java
@@ -51,15 +51,6 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import residence.HomeRole;
 import trace.AlertLog;
 import trace.AlertTag;
